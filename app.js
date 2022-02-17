d15f03da0a3ac5a47028ab91442579270e5bc9b5
Ian Coelho <ian.coelho@meliuz.com.br>
2022-02-17 18:34:38 -0300

    routing requests

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url
 
-  res.setHeader('Content-Type', 'text/html')
-  res.write('<html>')
-  res.write('<head><title>This is just a page</title></head>')
-  res.write('<body><h1>Hello from my node server</h1></body>')
-  res.write('</html>')
-  res.end()
+  if (url === '/') {
+    res.write('<html>')
+    res.write('<head><title>Enter message</title></head>')
+    res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></input></form></body>')
+    res.write('</html>')
+    return res.end()
+  }
+
+  
 });
 
 server.listen(3000);
