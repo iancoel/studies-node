e49d3d0803b0c83c91656083655004a633236270
Ian Coelho <ian.coelho@meliuz.com.br>
2022-02-19 19:27:23 -0300

    practice

--- routes.js
+++ routes.js
@@ -1,41 +1,41 @@
-const fs = require('fs')
-
 const requestHandler = (req, res) => {
-  const {url, method} = req
+  const {url, method} = req;
 
   if (url === '/') {
     res.write('<html>')
-    res.write('<head><title>Enter message</title></head>')
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></input></form></body>')
+    res.write('<head><title>Homepage</title></head>')
+    res.write('<body><h1>Hey, this is the homepage</h1></body>')
+    res.write('<form action="/create-user" method="POST">')
+    res.write('<input type="text" name="create-user">')
+    res.write('<button type="submit">Create a new user</button>')
+    res.write('</input>')
+    res.write('</form>')
     res.write('</html>')
-    return res.end()
+    return res.end();
   }
-  
-  if (url === '/message' && method === 'POST') {
-    const body = []
-    req.on('data', (chunk) => {
-      console.log(chunk)
-      body.push(chunk)
-    })
-  
+
+  if (url === '/users') {
+    res.write('<html>')
+    res.write('<head><title>Users</title></head>')
+    res.write('<body><ul><li>Monica</li><li>Cebolinha</li><li>Cascao</li><li>Magali</li><li>Franjinha</li></ul></body>')
+    res.write('</html>')
+    return res.end();
+  }
+
+  if (url === '/create-user' && method === "POST") {
+    const body = [];
+
+    req.on('data', (chunk) => body.push(chunk));
+
     req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString()
-      const message = parsedBody.split('=')[1]
-      console.log(message)
-      fs.writeFile('message.txt', message, (err) => {
-        res.statusCode = 302
-        res.setHeader('Location', '/')
-        return res.end()
-      })
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      console.log(message);
+      res.statusCode = 302;
+      res.setHeader('Location', '/users');
+      return res.end();
     })
   }
-  
-  res.setHeader('Content-Type', 'text/html')
-  res.write('<html>')
-  res.write('<head><title>My first page</title></head>')
-  res.write('<body><h1>Hello from my node server (:</h1></body>')
-  res.write('</html>')
-  return res.end()
 }
 
-module.exports = requestHandler
+module.exports = requestHandler;
