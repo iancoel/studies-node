8a4b215d0375a41c26a6d7105dcda6e656e8cb9e
Ian Coelho <ian.coelho@meliuz.com.br>
2022-02-20 15:43:22 -0300

    rootDir utils

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../utils/path');
+
 const router = express.Router();
 
 // admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))  
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+  console.log(rootDir);  
 });
 
 // admin/add-product => POST
