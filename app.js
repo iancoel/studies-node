93bfba0c9e97a118780bcdbc2d5ab899af577f01
Ian Coelho <ian.coelho@meliuz.com.br>
2022-02-20 15:32:49 -0300

    404 express

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -13,7 +15,7 @@ app.use(shopRoutes);
 
 //404 page
 app.use('/', (req, res, next) => {
-  res.status(404).send('<h2>Page not found</h2>')
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 app.listen(3000);
