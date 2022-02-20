4a0be5ff238dfc06de4e66eca3e495e6890aaceb
Ian Coelho <ian.coelho@meliuz.com.br>
2022-02-19 22:38:57 -0300

    initial express

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const http = require('http');
 
-const routes = require('./routes')
+const express = require('express');
 
-const server = http.createServer(routes);
+const app = express();
+
+// .use allows us to pass a new middleware function, the function you pass to it will be executed for every incoming request.
+// to allow it to travel on to the next middleware line, we have to call next()
+app.use((req, res, next) => {
+  console.log('hey this is the first middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('hey this is the second middleware');
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
