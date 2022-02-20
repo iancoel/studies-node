f3916cd7e065eaa23bfa808d29f9fd2141dd56c3
Ian Coelho <ian.coelho@meliuz.com.br>
2022-02-19 22:50:29 -0300

    sending responses

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
@@ -13,8 +11,8 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   console.log('hey this is the second middleware');
+
+  res.send('<h1>Hello from express</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
