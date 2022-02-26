3bfffb399fcd01ba624b101d391a9e62e5109894
Ian Coelho <ian.coelho@meliuz.com.br>
2022-02-26 12:39:56 -0300

    working with ejs

--- app.js
+++ app.js
@@ -2,16 +2,13 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}))// we use this to set a template engine that is not built-in like pug
-
-app.set('view engine', 'hbs'); //.set() allows to set any value globally on our application and we can read them with .get()
+app.set('view engine', 'ejs'); //.set() allows to set any value globally on our application and we can read them with .get()
 
 app.set('views', 'views'); //if we had to chenga the default directory from where our pages are provided, we would had to change this setting
 
