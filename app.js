8983f12e02b55c6b6f2f04e2fea4d6d30c1fcb76
Ian Coelho <ian.coelho@meliuz.com.br>
2022-02-26 12:08:55 -0300

    converting project to handlebars

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+app.engine('hbs', expressHbs())// we use this to set a template engine that is not built-in like pug
 
-app.set('view engine', 'pug') //.set() allows to set any value globally on our application and we can read them with .get()
+app.set('view engine', 'hbs'); //.set() allows to set any value globally on our application and we can read them with .get()
 
-app.set('views', 'views') //if we had to chenga the default directory from where our pages are provided, we would had to change this setting
+app.set('views', 'views'); //if we had to chenga the default directory from where our pages are provided, we would had to change this setting
 
 app.use(bodyParser.urlencoded({ extended: false })); //this registers a middleware that will do all the body parsing
 
--- routes/shop.js
+++ routes/shop.js
@@ -12,7 +12,12 @@ router.get('/', (req, res, next) => {
 
   const products = adminData.products
 
-  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'}) //render() will use the default template engine
+  res.render('shop', {
+    prods: products, 
+    pageTitle: 'Shop', 
+    path: '/', 
+    hasProducts: products.length > 0
+  }) //render() will use the default template engine
 });
 
 module.exports = router;
