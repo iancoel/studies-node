2cdac5b5c0631767957e248f44f8eaa26085b813
Ian Coelho <ian.coelho@meliuz.com.br>
2022-03-01 00:08:26 -0300

    loading product detail data

--- controllers/shop.js
+++ controllers/shop.js
@@ -13,6 +13,11 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId
+
+  Product.findById(prodId, product => {
+    console.log(product);
+  })
+
   res.redirect('/')
 }
 
